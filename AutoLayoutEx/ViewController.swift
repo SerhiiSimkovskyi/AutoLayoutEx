2a4c23ab34a0054ab71be191215eb56c714b1834
Serhii Simkovskyi <serhii.simkovskyi@gmail.com>
2018-02-13 14:00:56 +0200

    Initial Commit

---
+++ AutoLayoutEx/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutEx
+//
+//  Created by Serhii Simkovskyi on Feb/13/18.
+//  Copyright © 2018 Serhii Simkovskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
